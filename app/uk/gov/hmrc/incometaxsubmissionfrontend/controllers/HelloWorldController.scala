7f3f83341be7e433a6859bf52307f7563cd7b518
Jenkins user <jenkins@ip-10-35-66-202.eu-west-2.compute.internal>
2020-09-16 12:59:36 +0000

    Creating new service income-tax-submission-frontend

---
+++ app/uk/gov/hmrc/incometaxsubmissionfrontend/controllers/HelloWorldController.scala
@@ -0,0 +1,25 @@
+package uk.gov.hmrc.incometaxsubmissionfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.incometaxsubmissionfrontend.config.AppConfig
+import uk.gov.hmrc.incometaxsubmissionfrontend.views.html.HelloWorldPage
+
+import scala.concurrent.Future
+
+@Singleton
+class HelloWorldController @Inject()(
+  appConfig: AppConfig,
+  mcc: MessagesControllerComponents,
+  helloWorldPage: HelloWorldPage)
+    extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(helloWorldPage()))
+  }
+
+}
