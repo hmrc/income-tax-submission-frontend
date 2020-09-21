f92ced72392835a71090cb42080c78ea8e518647
dnyuar <60218795+dnyuar@users.noreply.github.com>
2020-09-21 16:36:44 +0100

    [SASS-26] start page

---
+++ test/views/StartPageViewSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+
+import config.AppConfig
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import views.html.StartPageView
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import play.twirl.api.Html
+
+
+class StartPageViewSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
+  object Selectors{
+    val individualPageHeading = "#main-content > div > div > main > div > h1"
+    val agentPageHeading = "#main-content > div > div > main > div > h1"
+    val provideUpdates = "#main-content > div > div > main > div > button"
+
+  }
+
+  val startPageView = app.injector.instanceOf[StartPageView]
+
+  implicit lazy val fakeRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("", "")
+  implicit lazy val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+  implicit lazy val messages: Messages = messagesApi.preferred(fakeRequest)
+  implicit lazy val mockConfig: AppConfig = app.injector.instanceOf[AppConfig]
+
+  def element(cssSelector: String)(implicit document: Document): Element = {
+    val elements = document.select(cssSelector)
+
+    if(elements.size == 0) {
+      fail(s"No element exists with the selector '$cssSelector'")
+    }
+
+    document.select(cssSelector).first()
+  }
+    def elementText(selector: String)(implicit document: Document): String = {
+    element(selector).text()
+  }
+  "Rendering the start page when the user is an individual" should {
+
+    lazy val view: Html = startPageView(false)(fakeRequest,messages,mockConfig)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "have the correct page heading" in {
+
+      elementText(Selectors.individualPageHeading) shouldBe "Tell HMRC about your Income Tax"
+    }
+  }
+
+  "rendering the start page when the user is an agent" should {
+    lazy val view: Html = startPageView(true)(fakeRequest,messages,mockConfig)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "have the correct page heading" in {
+      elementText(Selectors.agentPageHeading) shouldBe "Tell HMRC about your client’s Income Tax"
+    }
+  }
+
+  "have a 'provide updates' button" which {
+    lazy val view: Html = startPageView(true)(fakeRequest,messages,mockConfig)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "has the text 'provide updates" in {
+      elementText(Selectors.provideUpdates) shouldBe "Provide updates"
+
+    }
+  }
+
+
+
+
+}
