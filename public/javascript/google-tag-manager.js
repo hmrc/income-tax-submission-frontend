785e569033dadd7cbd103521933e7ab1efb18110
dnyuar <60218795+dnyuar@users.noreply.github.com>
2020-10-28 14:42:48 +0000

    [SASS-82] new line

--- public/javascript/google-tag-manager.js
+++ public/javascript/google-tag-manager.js
@@ -1,4 +1,3 @@
-
 // Google Tag Manager
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
     var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
