60a51714a4b6408dddd0061a250ae908f5ffdac2
Duane Miles <29195796+TheDeadlyPianist@users.noreply.github.com>
2020-11-23 11:37:35 +0000

    [DM] [genericSessionDataEndpoint] Added generic endpoint to add data to session

---
+++ app/controllers/testOnly/AdditionalSessionDataController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testOnly
+
+import config.FrontendAppConfig
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.OverviewPageView
+
+import scala.concurrent.Future
+
+class AdditionalSessionDataController @Inject()(
+                                                 appConfig: FrontendAppConfig,
+                                                 mcc: MessagesControllerComponents,
+                                                 overviewPageView: OverviewPageView) extends FrontendController(mcc) with I18nSupport {
+
+  implicit val config: FrontendAppConfig = appConfig
+
+  def show(taxYear: Int): Action[AnyContent] = Action.async { implicit request =>
+
+    val queryParams = request.queryString
+
+    lazy val result: Result = queryParams
+      .map(mapValue => mapValue._1 -> mapValue._2.head)
+      .foldRight(Redirect(controllers.routes.StartPageController.show(taxYear))) { (keyValuePair, passedResult) =>
+        val (key: String, value: String) = keyValuePair
+        passedResult.addingToSession(key -> value)
+      }
+
+    Future.successful(result)
+  }
+}
