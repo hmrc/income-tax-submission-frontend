2a12804a2d1f62ba51def32b8bde77ff67745322
Abdullah Juma <16039218+Dula-J@users.noreply.github.com>
2021-02-26 16:37:57 +0000

    SASS-301 - Updated service dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "2.25.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "3.4.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.26.0-play-27",
-    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.50.0-play-27"
+    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.63.0-play-27"
   )
 
   val test = Seq(
