fa17bfa45e84a5b29d015eb1efe089fcf597dd4c
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2021-05-14 12:08:05 +0100

    removed line

--- app/controllers/StartPageController.scala
+++ app/controllers/StartPageController.scala
@@ -53,14 +53,10 @@ class StartPageController @Inject()(val authorisedAction: AuthorisedAction,
           .addingToSession(SessionValues.TAX_YEAR -> taxYear.toString)
       )
 
-    //      Future.successful(
-    //        Ok(startPageView(isAgent = user.isAgent, taxYear))
-    //          .addingToSession(SessionValues.TAX_YEAR -> taxYear.toString)
     //          .removingFromSession(
     //            DIVIDENDS_CYA, INTEREST_CYA, GIFT_AID_CYA, EMPLOYMENT_CYA,
     //            DIVIDENDS_PRIOR_SUB, INTEREST_PRIOR_SUB, GIFT_AID_PRIOR_SUB, EMPLOYMENT_PRIOR_SUB
     //          ) //TODO Remove when year selection is available
-    //      )
   }
 
   def submit(taxYear: Int): Action[AnyContent] = (authorisedAction andThen taxYearAction(taxYear)).async { implicit user =>
