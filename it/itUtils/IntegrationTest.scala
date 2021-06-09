5efd4919085c105599879c1467237d3dd02d6782
ina-tu <74660663+ina-tu@users.noreply.github.com>
2021-06-09 14:49:19 +0100

    [SASS-897] - Update Pay model to align with the latest DES Spec

--- it/itUtils/IntegrationTest.scala
+++ it/itUtils/IntegrationTest.scala
@@ -143,7 +143,7 @@ trait IntegrationTest extends AnyWordSpecLike with Matchers with GuiceOneServerP
           directorshipCeasedDate = Some("2020-02-12"),
           occPen = Some(false),
           disguisedRemuneration = Some(false),
-          pay = Pay(34234.15, 6782.92, Some(67676), Some("CALENDAR MONTHLY"), Some("2020-04-23"), Some(32), Some(2))
+          pay = Some(Pay(Some(34234.15), Some(6782.92), Some(67676), Some("CALENDAR MONTHLY"), Some("2020-04-23"), Some(32), Some(2)))
         )),
         None
       )
