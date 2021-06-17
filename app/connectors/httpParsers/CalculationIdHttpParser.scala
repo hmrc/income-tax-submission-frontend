f14860145b417210c3195a0c8e8b780caf1a6ef2
JuamalBlackman <juamal.blackman@digital.hmrc.gov.uk>
2021-06-17 10:21:47 +0100

    [SASS-900] added connector 422/UNPROCESSSABLE ENTITY

--- app/connectors/httpParsers/CalculationIdHttpParser.scala
+++ app/connectors/httpParsers/CalculationIdHttpParser.scala
@@ -19,7 +19,7 @@ package connectors.httpParsers
 import models.{APIErrorModel, LiabilityCalculationIdModel}
 import play.api.http.Status._
 import uk.gov.hmrc.http.{HttpReads, HttpResponse}
-import utils.PagerDutyHelper.PagerDutyKeys.{FOURXX_RESPONSE_FROM_API, _}
+import utils.PagerDutyHelper.PagerDutyKeys.{FOURXX_RESPONSE_FROM_API, UNPROCESSABLE_ENTITY_FROM_DES, _}
 import utils.PagerDutyHelper.pagerDutyLog
 
 object CalculationIdHttpParser extends APIParser {
@@ -43,6 +43,9 @@ object CalculationIdHttpParser extends APIParser {
           handleAPIError(response)
         case BAD_REQUEST | NOT_FOUND | CONFLICT =>
           pagerDutyLog(FOURXX_RESPONSE_FROM_API, logMessage(response))
+          handleAPIError(response)
+        case UNPROCESSABLE_ENTITY =>
+          pagerDutyLog(UNPROCESSABLE_ENTITY_FROM_DES, logMessage(response))
           handleAPIError(response)
         case _ =>
           pagerDutyLog(UNEXPECTED_RESPONSE_FROM_API, logMessage(response))
