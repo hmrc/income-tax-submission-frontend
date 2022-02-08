bf64e24845d081958cfb07d8bbe49a447443a8fb
dnyuar <60218795+dnyuar@users.noreply.github.com>
2022-02-08 10:39:09 +0000

    content changes to start page

--- it/controllers/StartPageControllerISpec.scala
+++ it/controllers/StartPageControllerISpec.scala
@@ -64,37 +64,33 @@ class StartPageControllerISpec extends IntegrationTest with ViewHelpers {
     val useThisServiceTextWelsh = "Defnyddiwch y gwasanaeth hwn i ddiweddaru a chyflwyno Ffurflen Dreth Incwm."
     val newServiceText = "This is a new service. At the moment you can only update information about:"
     val newServiceTextWelsh = "Mae hwn yn wasanaeth newydd. Ar hyn o bryd, gallwch ond diweddaru gwybodaeth am y canlynol:"
+    val bullet1UkInterestPaidText = "UK interest"
+    val bullet1UkInterestPaidTextWelsh = "llog y DU"
     val bullet2DividendsFromUKText = "dividends from UK companies, trusts and open-ended investment companies"
     val bullet2DividendsFromUKTextWelsh = "difidendau gan gwmnïau, ymddiriedolaethau a chwmnïau buddsoddi penagored yn y DU"
+    val bullet3DonationsToCharityText = "donations to charity"
+    val bullet3DonationsToCharityTextWelsh = "rhoddion i elusennau"
     val continueButtonText = "Continue"
     val continueButtonTextWelsh = "Welsh"
     val continueButtonHref = s"/update-and-submit-income-tax-return/$taxYear/start"
   }
 
   object IndividualExpectedResultsEN {
-    val bullet1InterestPaidIndividualText = "interest paid to you in the UK"
-    val bullet3DonationsToCharityIndividual = "your donations to charity"
     val viewEmploymentInformationIndividual = "You can view your employment information but cannot use this service to update it until 6 April 2022."
     val toUpdateIncomeIndividualText = "To update your self-employment and property income, you must use your chosen commercial software."
   }
 
   object IndividualExpectedResultsCY {
-    val bullet1InterestPaidIndividualText = "llog a dalwyd i chi yn y DU"
-    val bullet3DonationsToCharityIndividual = "eich rhoddion chi i elusennau"
     val viewEmploymentInformationIndividual = "Gallwch weld eich gwybodaeth am gyflogaeth ond ni allwch ddefnyddio’r gwasanaeth hwn i’w diweddaru tan 6 Ebrill 2022."
     val toUpdateIncomeIndividualText = "I ddiweddaru’ch incwm o hunangyflogaeth a’ch incwm o eiddo, mae’n rhaid i chi ddefnyddio’ch meddalwedd fasnachol ddewisedig."
   }
 
   object AgentExpectedResultsEN {
-    val bullet1InterestPaidAgentText = "interest paid to your client in the UK"
-    val bullet3DonationsToCharityAgent = "your client’s donations to charity"
     val viewEmploymentInformationAgent = "You can view your client’s employment information but cannot use this service to update it until 6 April 2022."
     val toUpdateIncomeAgentText = "To update your client’s self-employment and property income, you must use your chosen commercial software."
   }
 
   object AgentExpectedResultsCY {
-    val bullet1InterestPaidAgentText = "llog a dalwyd i’ch cleient yn y DU"
-    val bullet3DonationsToCharityAgent = "rhoddion eich cleient i elusennau"
     val viewEmploymentInformationAgent = "Gallwch weld gwybodaeth am gyflogaeth eich cleient ond ni allwch ddefnyddio’r gwasanaeth hwn i’w diweddaru tan 6 Ebrill 2022."
     val toUpdateIncomeAgentText = "I ddiweddaru incwm o hunangyflogaeth ac incwm o eiddo eich cleient, mae’n rhaid i chi ddefnyddio’ch meddalwedd fasnachol ddewisedig."
   }
@@ -146,9 +142,9 @@ class StartPageControllerISpec extends IntegrationTest with ViewHelpers {
       textOnPageCheck(caption, Selectors.caption)
       textOnPageCheck(useThisServiceText, Selectors.p1)
       textOnPageCheck(newServiceText, Selectors.p2)
-      textOnPageCheck(bullet1InterestPaidIndividualText, Selectors.bullet1)
+      textOnPageCheck(bullet1UkInterestPaidText, Selectors.bullet1)
       textOnPageCheck(bullet2DividendsFromUKText, Selectors.bullet2)
-      textOnPageCheck(bullet3DonationsToCharityIndividual, Selectors.bullet3)
+      textOnPageCheck(bullet3DonationsToCharityText, Selectors.bullet3)
       textOnPageCheck(viewEmploymentInformationIndividual, Selectors.p3)
       textOnPageCheck(toUpdateIncomeIndividualText, Selectors.p4)
       formPostLinkCheck(continueButtonHref, Selectors.continueButton)
@@ -178,9 +174,9 @@ class StartPageControllerISpec extends IntegrationTest with ViewHelpers {
       textOnPageCheck(caption, Selectors.caption)
       textOnPageCheck(useThisServiceText, Selectors.p1)
       textOnPageCheck(newServiceText, Selectors.p2)
-      textOnPageCheck(bullet1InterestPaidAgentText, Selectors.bullet1)
+      textOnPageCheck(bullet1UkInterestPaidText, Selectors.bullet1)
       textOnPageCheck(bullet2DividendsFromUKText, Selectors.bullet2)
-      textOnPageCheck(bullet3DonationsToCharityAgent, Selectors.bullet3)
+      textOnPageCheck(bullet3DonationsToCharityText, Selectors.bullet3)
       textOnPageCheck(viewEmploymentInformationAgent, Selectors.p3)
       textOnPageCheck(toUpdateIncomeAgentText, Selectors.p4)
       formPostLinkCheck(continueButtonHref, Selectors.continueButton)
@@ -213,9 +209,9 @@ class StartPageControllerISpec extends IntegrationTest with ViewHelpers {
       textOnPageCheck(captionWelsh, Selectors.caption)
       textOnPageCheck(useThisServiceTextWelsh, Selectors.p1)
       textOnPageCheck(newServiceTextWelsh, Selectors.p2)
-      textOnPageCheck(bullet1InterestPaidIndividualText, Selectors.bullet1)
+      textOnPageCheck(bullet1UkInterestPaidTextWelsh, Selectors.bullet1)
       textOnPageCheck(bullet2DividendsFromUKTextWelsh, Selectors.bullet2)
-      textOnPageCheck(bullet3DonationsToCharityIndividual, Selectors.bullet3)
+      textOnPageCheck(bullet3DonationsToCharityTextWelsh, Selectors.bullet3)
       textOnPageCheck(viewEmploymentInformationIndividual, Selectors.p3)
       textOnPageCheck(toUpdateIncomeIndividualText, Selectors.p4)
       formPostLinkCheck(continueButtonHref, Selectors.continueButton)
@@ -245,9 +241,9 @@ class StartPageControllerISpec extends IntegrationTest with ViewHelpers {
       textOnPageCheck(captionWelsh, Selectors.caption)
       textOnPageCheck(useThisServiceTextWelsh, Selectors.p1)
       textOnPageCheck(newServiceTextWelsh, Selectors.p2)
-      textOnPageCheck(bullet1InterestPaidAgentText, Selectors.bullet1)
+      textOnPageCheck(bullet1UkInterestPaidTextWelsh, Selectors.bullet1)
       textOnPageCheck(bullet2DividendsFromUKTextWelsh, Selectors.bullet2)
-      textOnPageCheck(bullet3DonationsToCharityAgent, Selectors.bullet3)
+      textOnPageCheck(bullet3DonationsToCharityTextWelsh, Selectors.bullet3)
       textOnPageCheck(viewEmploymentInformationAgent, Selectors.p3)
       textOnPageCheck(toUpdateIncomeAgentText, Selectors.p4)
       formPostLinkCheck(continueButtonHref, Selectors.continueButton)
