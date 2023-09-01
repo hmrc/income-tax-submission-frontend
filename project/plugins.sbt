fce63b6fd50fbbcc8265d0b579e46c95c40d23e0
Hassan Shahab <4068568+hs3t@users.noreply.github.com>
2023-09-01 10:32:57 +0100

    NO-TICKET: Removed MaxPermSize warning and test plugin upgrade

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -14,7 +14,7 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.19")
 
-addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.5.1")
+addSbtPlugin("io.github.irundaia" % "sbt-sassify" % "1.5.2")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.6")
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt run -Dconfig.resource=application.conf -Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Dplay.akka.http.server.request-timeout=60s -J-Xmx256m -J-Xms64m -J-XX:MaxPermSize=128m -Dhttp.port=9302 -Drun.mode=Dev
+sbt run -Dconfig.resource=application.conf -Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Dplay.akka.http.server.request-timeout=60s -J-Xmx256m -J-Xms64m -Dhttp.port=9302 -Drun.mode=Dev
