8e3f9b69d80285e07f571f2e25e743ee4ffa0588
iyadi <7164084+iyadi@users.noreply.github.com>
2025-05-27 18:45:28 +0100

    SASS-11542: Common task list endpoint

--- app/viewmodels/TaskListPageViewModel.scala
+++ app/viewmodels/TaskListPageViewModel.scala
@@ -206,7 +206,7 @@ case class TaskListPageViewModel @Inject()(taskListData: Option[TaskListModel], 
       SelfEmploymentTitle.toString -> messages(prefix + SELF_EMPLOYMENT_TITLE_KEY),
       SelfEmploymentTitle.toString -> {
         if(params.isEmpty){
-          "Self-Employment" //this is a temp fix for CIS until the proper fix. It will not work for Welsh
+          messages("Self-Employment") //this is a temp fix for CIS until the proper fix. It will not work for Welsh
         } else {
           val msg = messages(prefix + SELF_EMPLOYMENT_TITLE_KEY, params: _*)
           if (msg.isEmpty) messages(prefix + "noTradingName") else msg
